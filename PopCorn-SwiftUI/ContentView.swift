5f2fb11d85025d6bb5ed23143ba5569abd0da4c6
valmirt <valmirt.dev@gmail.com>
2021-04-28 23:18:17 -0300

    Initial Commit

---
+++ PopCorn-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PopCorn-SwiftUI
+//
+//  Created by Valmir Junior on 28/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PopCorn-SwiftUI/PopCorn_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopCorn_SwiftUIApp.swift
+//  PopCorn-SwiftUI
+//
+//  Created by Valmir Junior on 28/04/21.
+//
+
+import SwiftUI
+
+@main
+struct PopCorn_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
